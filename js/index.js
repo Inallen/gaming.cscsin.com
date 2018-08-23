72e8b44ea9be978adebdd02b3b328a19123adb1f
Jing Zhu <jing.zhu@mdpi.com>
2018-08-23 18:05:10 +0800

    add adapter for score

--- js/index.js
+++ js/index.js
@@ -1,8 +1,45 @@
 $(function () {
     const BASE_API_URL = "http://1zplay.com/api/";
     const LIVE_SCHEDULE = BASE_API_URL + "live_schedules"
+
+    var itemManager = {
+        _defaultItem: null,
+        _defaultType: null,
+        setItem: function(item) {
+            this._defaultItem = item;
+            this._init();
+            return this;
+        },
+        _init: function() {
+            console.log(this._defaultItem);
+            if (this._defaultItem.hasOwnProperty("csgo_schedule")) {
+                this._defaultType = "csgo";
+            }
+            else if (this._defaultItem.hasOwnProperty("csgo_schedule")) {
+                this._defaultType = "dota2";
+            }
+            return this;
+        },
+        getItemScore: function(team) {
+            if (this._defaultType == "csgo") {
+                var team_name = team == "left_team" ? "team1" : "team2";
+                if (this._defaultItem.hasOwnProperty("live_match")) {
+                    return this._defaultItem['live_match'][team_name + "_score"];
+                }
+                else {
+                    return 0;
+                }
+            }
+            else if(this._defaultType == "dota2") {
+                return this._defaultItem['live_match']['left_data']['score']
+            }
+        },
+        getItemName: function() {
+
+        }
+    };
     var timeStamp = new Date().getTime();
-    var category = "dota2";
+    var category = "csgo";
     var data = {
         "_" : timeStamp,
         "category" : category
@@ -21,9 +58,9 @@ $(function () {
         itemBody += '</div>';
         itemBody += '<div class="weui-cell__bd">';
         itemBody += '<p style="float:left">' + item['left_team']['name'] + '</p>';
-        itemBody += '<p style="float:left">' + item['live_match']['left_data']['score'] + '</p>';
+        itemBody += '<p style="float:left">' + itemManager.setItem(item).getItemScore("left_team") + '</p>';
         itemBody += '<p style="float:right">' + item['right_team']['name'] + '</p>';
-        itemBody += '<p style="float:right">' + item['live_match']['right_data']['score'] + '</p>';
+        itemBody += '<p style="float:right">' + itemManager.setItem(item).getItemScore("right_team") + '</p>';
         itemBody += '</div>';
         itemBody += '<div class="weui-cell__ft">';
         itemBody += '<img src="' + item['right_team']['logo'] + '" style="width: 50px;display: block"/>';
